e31cb27f3bf97fcff00d223d690b2699a33bfae5
Mariana Granados <mgranados09@alumnos.uaq.mx>
2023-04-21 01:22:34 -0600

    working with one analog channel, Vx

--- code1.c
+++ code1.c
@@ -1,43 +1,30 @@
-#Include <18F4550.h>
+#include <18F4550.h>
 #device adc=10
 #Fuses INTRC,NOPROTECT, NOWDT, CPUDIV1, PLL1
 #Use Delay(Clock=8M)
-#Use RS232(RCV=PIN_C7, XMIT=PIN_C6, Baud=9600, Bits=8, Stream = TTL)
+#use RS232(RCV=PIN_C7, XMIT=PIN_C6, Baud=9600, Bits=8)
 
-
-void main()
+int main(void)
 {
-   int16 valor0, valor1;
-   //float conv0, conv1;
+   int value0;
+   float conv0;
    
-   setup_adc_ports(AN0_TO_AN1 );  //activar canales analogicos
-   setup_adc(ADC_CLOCK_INTERNAL); //fuente de reloj rc
-   printf(TTL, "Puertos analogicos configurados\n");
+   setup_adc_ports(AN0);
+   setup_adc(ADC_CLOCK_INTERNAL);
+   printf("Analog ports configured\n");
    delay_ms(500);
    
-
-   
-   while(true)
-   {
-      
+   for(;;)
+   {   
       set_adc_channel(0);
-      delay_us(20); //espera necesaria al cambiar de canal
-      valor0 = read_adc(); //leer canal 0
-      //conv0 = 5.0 * valor0 / 1024.0;
+      delay_ms(20);
+      value0 = read_adc();
+      conv0 = 5.0 * value0 / 1024.0;
       
-      set_adc_channel(1);
-      delay_us(20); //espera necesaria al cambiar de canal
-      valor1 = read_adc(); //leer canal 1
-      //conv1 = 5.0 * valor1 / 1024.0;
-      
-      printf(TTL, "A0: %41d \n", valor0);
-      printf(TTL, "A1: %41d", valor1);
-      delay_ms(500);
-
-   
+      printf("A0: %d\n", value0);
+      printf("Voltage: %01.2f V\n", conv0);
+      delay_ms(2000);
    }
 
-
+   return 0;
 }
-
-
